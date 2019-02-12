23b95ccb3646322209112de6800a1a95ff7245ea
Andrey Smirnov <twintetra@gmail.com>
2019-02-12 17:59:11 +0300

    add all css from tutorial

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import ToDoListItem from './todo-list-item';
 
+import TodoListItem from './todo-list-item';
+import './todo-list.css';
 
-const ToDoList = ({todos}) => {
+const TodoList = ({ todos }) => {
 
-    const elements =  todos.map((item) => {
-        const { id, ...itemProps } = item;
-        return (
-            <li key={id} className="list-group-item">
-                <ToDoListItem { ...itemProps}/>
-            </li>
-        )
-    });
-
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
 
     return (
-        <ul className="list-group todo-list">
-            { elements }
-        </ul>
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
+      </li>
     );
+  });
+
+  return (
+    <ul className="list-group todo-list">
+      { elements }
+    </ul>
+  );
 };
 
-export default ToDoList;
+export default TodoList;
