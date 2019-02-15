bd3e45b9aeaf346f7d0b5bc09c344a0c525fb9f2
Andrey Smirnov <twintetra@gmail.com>
2019-02-15 16:03:54 +0300

    state 2

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -9,10 +9,14 @@ export default class TodoListItem extends Component {
     constructor() {
       super();
       this.state = {
-          done: false
+          done: false,
+          important: false
       };
       this.onLabelClick = () => {
-         this.setState({done: true});
+         this.setState({ done: true });
+      };
+      this.onMarkImportant = () => {
+          this.setState({ important: true })
       };
   }
 
@@ -20,18 +24,16 @@ export default class TodoListItem extends Component {
  render() {
 
 
-     const { label, important = false } = this.props;
-     const { done } = this.state;
+     const { label } = this.props;
+     const { done, important } = this.state;
 
      let classNames = 'todo-list-item';
      if (done) {
          classNames += ' done';
      }
-
-     const style = {
-         color: important ? 'steelblue' : 'black',
-         fontWeight: important ? 'bold' : 'normal'
-     };
+     if (important) {
+         classNames += ' important';
+     }
 
 
      return (
@@ -39,14 +41,15 @@ export default class TodoListItem extends Component {
     <span className={classNames}>
       <span
           className="todo-list-item-label"
-          style={style}
           onClick={ this.onLabelClick }
       >
         {label}
       </span>
 
       <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
+              className="btn btn-outline-success btn-sm float-right"
+              onClick={ this.onMarkImportant}
+      >
         <i className="fa fa-exclamation" />
       </button>
 
