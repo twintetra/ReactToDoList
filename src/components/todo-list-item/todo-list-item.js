eb30420cb77e5431ec02432c51fd6e8522bc5b56
Andrey Smirnov <twintetra@gmail.com>
2019-02-15 18:06:45 +0300

    state (func)

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -13,10 +13,18 @@ export default class TodoListItem extends Component {
           important: false
       };
       this.onLabelClick = () => {
-         this.setState({ done: true });
+         this.setState(({done}) => {
+             return {
+                 done: !done
+             }
+         });
       };
       this.onMarkImportant = () => {
-          this.setState({ important: true })
+          this.setState(({important}) => {
+              return {
+                  important: !important
+              }
+          })
       };
   }
 
