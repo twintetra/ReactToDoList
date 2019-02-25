3fe8898c7e5d7ee98c34d7f7f6d39404910ccd4d
Andrey Smirnov <twintetra@gmail.com>
2019-02-25 12:33:00 +0300

    own event

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -25,7 +25,10 @@ const App = () => {
                 <ItemStatusFilter />
             </div>
 
-            <TodoList todos={todoData} />
+            <TodoList
+                todos={todoData}
+                onDeleted={(id)=> console.log('del ', id)}
+            />
         </div>
     );
 };
